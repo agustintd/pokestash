248f7e260a4abd3b4d189b94453fad43bc25f553
Agustin <agustintottil12@gmail.com>
2024-04-26 23:00:10 -0300

    Update 6

--- app/components/cardImg.js
+++ app/components/cardImg.js
@@ -1,15 +1,13 @@
 "use client"
 //import { pokeids } from './pokeids';
 import { useState, useEffect } from 'react';
-import CardSkeleton from './cardskeleton';
-export default function Card() {
+export default function CardImg() {
     const [imgLoaded, setImgLoaded] = useState(false);
     const pokeids = ["hgss4-1", "xy5-1"];
     const [cardinfo, setCardinfo] = useState(["none","none","none", "none"]);
     const randomIndex = Math.floor(Math.random() * pokeids.length);
     const randomId = pokeids[randomIndex]; 
-    const idUnico = 1;
-    let displayValue = "block"
+    const idUnico = "1";
     function generarIdUnico() {
         let cardId = localStorage.getItem('cardId');
         if (!cardId) {
@@ -43,7 +41,7 @@ export default function Card() {
     }
 
     function handleImageLoaded(){
-        displayValue = "none"
+        document.getElementById(idUnico+"l").removeAttribute('style');
         document.getElementById(idUnico).removeAttribute('style');
     }
 
@@ -53,16 +51,8 @@ export default function Card() {
     return (
         <div className='card-container'>
             <div className='centerxy h-[279px] w-[200px]'>
-                <CardSkeleton style={displayValue} id={idUnico+"l"}></CardSkeleton>
+                <div style={{display: "block"}} id={idUnico+"l"} className='loader'></div>
                 <img style={{display: "none"}} src={cardinfo[1]} id={idUnico} className='h-[279px] w-[200px] text-white' onLoad={handleImageLoaded}></img>
-            </div>
-
-            <div className='topDiplay'>
-                <h1 className=''>{cardinfo[0]}</h1>
-            </div>
-            <div className='bottom-diplay'>
-                <span className='cardNumber'>{cardinfo[3]}</span>
-                <span className='cardPrice'> {cardinfo[2]}</span>
             </div>
         </div>
     );
