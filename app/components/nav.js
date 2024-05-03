d6d591077c38547a9b8e8025cbb5c5795698edfd
Joshua <joshuabalaiche@gmail.com>
2024-05-03 01:02:06 -0300

    Estilo en sets y pestania codes

--- app/components/nav.js
+++ app/components/nav.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Nav() { 
     return (
-        <div className="w-[100%] h-[70px] scolor centerxy mr-auto ml-auto text-xl"> 
+        <div className="w-[100%] h-[70px] scolor centerxy mr-auto ml-auto text-xl border-b border-gray-600"> 
             <div id="desktop-nav" className="w-[50%] h-[75%] min-w-[500px] max-w-[1500px] centerxy gap-20">
                 <div className=""> <Link href="/"><p> Logo</p></Link> </div>
                 <div className=""> <Link href="/sets"><p> SETS </p></Link></div>
